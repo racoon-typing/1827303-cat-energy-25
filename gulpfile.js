53e5669e558513778ccb19f84a28e7cb9d11fb35
Daniil <eo18622@yandex.ru>
2022-05-25 15:19:08 +0300

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,14 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import del from 'del';
+import svgo from 'gulp-svgmin';
+
 import browser from 'browser-sync';
+
+
 
 // Styles
 
@@ -38,6 +45,51 @@ const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
+
+// Copy
+
+const copy = (done) => {
+gulp.src([
+'source/fonts/*.{woff2,woff}',
+'source/*.ico',
+'source/*.html',
+'source/*.webmanifest',
+'source/*.js'
+], {
+base: 'source'
+})
+.pipe(gulp.dest('build'))
+done();
+}
+
+// Clean
+
+export const clean = () => {
+return del('build');
+};
+
+
+// StylesMin
+
+const stylesMin = () => {
+return gulp.src('source/less/style.less', { sourcemaps: true })
+.pipe(plumber())
+.pipe(less())
+.pipe(postcss([
+autoprefixer(),
+csso()
+]))
+.pipe(rename('style.min.css'))
+.pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+.pipe(browser.stream());
+}
+
+// SVG
+
+export const svg = () =>
+gulp.src(['source/img/**/*.svg', '!source/img/icons/*.svg', '!source/img/*.svg'])
+.pipe(svgo())
+.pipe(gulp.dest('build/img/svg'));
 
 
 export default gulp.series(
